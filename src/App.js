92475b135a95ec2e983a989f0099a9568524c17a
thuan <tvc4567@gmail.com>
2022-07-12 10:54:39 +0700

    test22222

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     
       await fetch(serverUrl, {
         method: "GET",
-        credentials: "include",
+        credentials: "same-origin",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
