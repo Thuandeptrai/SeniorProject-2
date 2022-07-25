0c69763241a3458bb5051ef58d81645fbefb7262
thuan <tvc4567@gmail.com>
2022-07-25 19:34:35 +0700

    This is oke

--- src/pages/CreateProb.js
+++ src/pages/CreateProb.js
@@ -1,158 +1,253 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Field, Form, FieldArray } from "formik";
 import { CreateProbSchema } from "../validate/validation";
+import axios from "axios";
 
 function CreateProb() {
+  const hanldeSubmit = async (values) => {
+    let testOutput = [];
+    let testInput = [];
+    let realInput = [];
+    let realOutput = [];
+    for (let i = 0; i < values.realtInputAndOutPut.length; i++) {
+      realInput.push(values.realtInputAndOutPut[i].realInput);
+      realOutput.push(values.realtInputAndOutPut[i].realOutput);
+    }
+    for (let i = 0; i < values.testInputAndOutPut.length; i++) {
+      testInput.push(values.testInputAndOutPut[i].testInput);
+      testOutput.push(values.testInputAndOutPut[i].testOutput);
+    }
+    const creatProb = axios.create({
+      withCredentials: true,
+    });
+    await creatProb
+      .post("http://localhost:3001/createProblem", {
+        title: values.Title,
+        desc: values.Description,
+        testInput,
+        testOutput,
+        realInput,
+        realOutput,
+        isPrivate: values.isPrivate,
+      })
+      .then((data) => {
+        console.log(data.data);
+      });
+  };
   return (
     <>
-      <div className="flex flex-col items-center bg-white px-5 py-9 border border-gray-primary mb-3 rounded">
-        <h1 className="flex justify-center w-full">1</h1>
-        <p className="text-gray-400 text-center font-semibold mb-5">
-          Sign up to see photos and videos from your friends.
-        </p>
+      <div className="container flex mx-auto max-w-screen-lg items-center justify-center h-screen">
+        <div className="flex flex-col width-screen items-center bg-white px-5 py-9 border border-gray-primary mb-3 rounded">
+          <h1 className="flex justify-center w-full text-gray-400 text-center font-semibold mb-5">
+            {" "}
+            Create Problem
+          </h1>
 
-        <Formik
-          validationSchema={CreateProbSchema}
-          initialValues={{
-            Title: "",
-            Description: "",
-            testInput: [""],
-            testOutput: [""],
-            isPrivate: false,
-            realInput: [""],
-            realOutput: [""],
-          }}
-          onSubmit={async (values, { resetForm, setSubmitting }) => {
-            await new Promise((r) => setTimeout(r, 500));
-            alert(JSON.stringify(values, null, 2));
-            console.log(values);
-          }}
-        >
-          {({ isSubmitting, isValid, errors, remove, values, touched }) => (
-            <>
-              {console.log(errors)}
-              <Form className="w-full grid grid-cols-1 gap-4">
-                <label htmlFor="Title">Title</label>
-                <Field name="Title" placeholder="Title" />
-                <label htmlFor="Description">Description</label>
-                <Field name="Description" placeholder="Description" />
-                <FieldArray name="testInput">
-                  {({ insert, remove, push }) => (
+          <Formik
+            validationSchema={CreateProbSchema}
+            initialValues={{
+              Title: "",
+              Description: "",
+              testInputAndOutPut: [
+                {
+                  testOutput: "",
+                  testInput: "",
+                },
+              ],
+
+              isPrivate: false,
+              realtInputAndOutPut: [
+                {
+                  realInput: "",
+                  realOutput: "",
+                },
+              ],
+            }}
+            onSubmit={async (values, { resetForm, setSubmitting }) => {
+              console.log(values);
+              try {
+                await hanldeSubmit(values);
+              } finally {
+                setSubmitting(false);
+                resetForm();
+              }
+            }}
+          >
+            {({ isSubmitting, isValid, errors, remove, values, touched }) => (
+              <>
+                <Form className="w-full grid grid-cols-1 gap-4 place-items-stretch">
+                  <Field
+                    name="Title"
+                    placeholder="Title"
+                    className="text-sm focus:ring-gray-700 focus:border-gray-400 text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                  />
+                  {errors?.Title !== undefined && touched?.Title ? (
                     <>
-                      <div>
-                        {values.testInput.length > 0 &&
-                          values.testInput.map((testInput, index) => (
-                            <>
-                              <div key={index}>
-                                <Field
-                                  name={`testInput.${index}`}
-                                  placeholder="Test Input"
-                                  type="text"
-                                />
-                              </div>
-                            </>
-                          ))}
-                        <button
-                          type="button"
-                          className="secondary"
-                          onClick={() => push("")}
-                        >
-                          Add Visible Input Testcase
-                        </button>
-                      </div>
+                      {" "}
+                      <p>{errors.Title}</p>
                     </>
-                  )}
-                </FieldArray>
-                <FieldArray name="testOutput">
-                  {({ insert, remove, push }) => (
+                  ) : null}
+
+                  <Field
+                    name="Description"
+                    placeholder="Description"
+                    className="text-sm focus:ring-gray-700 focus:border-gray-400 text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                  />
+                  {errors?.Description !== undefined && touched?.Description ? (
                     <>
-                      <div>
-                        {values.testOutput.length > 0 &&
-                          values.testOutput.map((testOutput, index) => (
-                            <>
-                              <div key={index}>
-                                <Field
-                                  name={`testOutput.${index}`}
-                                  placeholder="Test Output"
-                                  type="text"
-                                />
-                              </div>
-                            </>
-                          ))}
-                        <button
-                          type="button"
-                          className="secondary"
-                          onClick={() => push("")}
-                        >
-                          Add Visible Out Testcase
-                        </button>
-                      </div>
+                      {" "}
+                      <p>{errors.Description}</p>
                     </>
-                  )}
-                </FieldArray>
-                <FieldArray name="realInput">
-                  {({ insert, remove, push }) => (
-                    <>
-                      <div>
-                        {values.realInput.length > 0 &&
-                          values.realInput.map((realInput, index) => (
-                            <>
-                              <div key={index}>
-                                <Field
-                                  name={`realInput.${index}`}
-                                  placeholder="Real Input"
-                                  type="text"
-                                />
-                              </div>
-                            </>
-                          ))}
-                        <button
-                          type="button"
-                          className="secondary"
-                          onClick={() => push("")}
-                        >
-                          Add Unvisible Input Testcase
-                        </button>
-                      </div>
-                    </>
-                  )}
-                </FieldArray>
-                <FieldArray name="realOutput">
-                  {({ insert, remove, push }) => (
-                    <>
-                      <div>
-                        {values.realOutput.length > 0 &&
-                          values.realOutput.map((realOutput, index) => (
-                            <>
-                              <div key={index}>
-                                <Field
-                                  name={`realOutput.${index}`}
-                                  placeholder="Real Output"
-                                  type="text"
-                                />
-                              </div>
-                            </>
-                          ))}
-                        <button
-                          type="button"
-                          className="secondary"
-                          onClick={() => push("")}
-                        >
-                          Add Unvisible Output Testcase
-                        </button>
-                      </div>
-                    </>
-                  )}
-                </FieldArray>
-                <label>
-                  <Field type="checkbox" name="isPrivate" />
-                  {`${values.isPrivate}`}
-                </label>
-                <button type="submit">Invite</button>
-              </Form>
-            </>
-          )}
-        </Formik>
+                  ) : null}
+                  <FieldArray name="testInputAndOutPut">
+                    {({ insert, remove, push }) => (
+                      <>
+                        <div>
+                          {values.testInputAndOutPut.length > 0 &&
+                            values.testInputAndOutPut.map(
+                              (testInputAndOutPut, index) => (
+                                <>
+                                  <div key={index}>
+                                    <Field
+                                      name={`testInputAndOutPut.${index}.testInput`}
+                                      placeholder="Test Input"
+                                      type="text"
+                                      className="text-sm focus:ring-gray-700 focus:border-gray-400 text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                                    />
+
+                                    <Field
+                                      name={`testInputAndOutPut.${index}.testOutput`}
+                                      placeholder="Test Output"
+                                      type="text"
+                                      className="text-sm focus:ring-gray-700 focus:border-gray-400 text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                                    />
+                                    {errors?.testInputAndOutPut !== undefined &&
+                                    touched?.testInputAndOutPut ? (
+                                      <>
+                                        {touched.testInputAndOutPut[index] ? (
+                                          <p>
+                                            {
+                                              errors.testInputAndOutPut[index]
+                                                ?.testOutput
+                                            }
+                                          </p>
+                                        ) : null}
+
+                                        {touched.testInputAndOutPut[index] ? (
+                                          <p>
+                                            {
+                                              errors.testInputAndOutPut[index]
+                                                ?.testInput
+                                            }
+                                          </p>
+                                        ) : null}
+                                      </>
+                                    ) : null}
+                                  </div>
+                                </>
+                              )
+                            )}
+                          <button
+                            type="button"
+                            className="secondary"
+                            onClick={() => {
+                              push({ testOutput: "", testInput: "" });
+                            }}
+                          >
+                            Add Visible Input Testcase
+                          </button>
+                        </div>
+                      </>
+                    )}
+                  </FieldArray>
+                  <FieldArray name="realtInputAndOutPut">
+                    {({ insert, remove, push }) => (
+                      <>
+                        <div>
+                          {values.realtInputAndOutPut.length > 0 &&
+                            values.realtInputAndOutPut.map(
+                              (realtInputAndOutPut, index) => (
+                                <>
+                                  <div key={index}>
+                                    <Field
+                                      name={`realtInputAndOutPut.${index}.realInput`}
+                                      placeholder="Test Input"
+                                      type="text"
+                                      className="text-sm focus:ring-gray-700 focus:border-gray-400 text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                                    />
+
+                                    <Field
+                                      name={`realtInputAndOutPut.${index}.realOutput`}
+                                      placeholder="Test Output"
+                                      type="text"
+                                      className="text-sm focus:ring-gray-700 focus:border-gray-400 text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                                    />
+                                  </div>
+                                  {errors?.realtInputAndOutPut !== undefined &&
+                                  touched?.realtInputAndOutPut ? (
+                                    <>
+                                      {touched.realtInputAndOutPut[index] ? (
+                                        <p>
+                                          {
+                                            errors.realtInputAndOutPut[index]
+                                              ?.realInput
+                                          }
+                                        </p>
+                                      ) : null}
+
+                                      {touched.realtInputAndOutPut[index] ? (
+                                        <p>
+                                          {
+                                            errors.realtInputAndOutPut[index]
+                                              ?.realOutput
+                                          }
+                                        </p>
+                                      ) : null}
+                                    </>
+                                  ) : null}
+                                  {/* {errors.realtInputAndOutPut !== undefined  ? (
+                                  <>
+                                    <p className="mb-3 pl-1 text-xs text-red-primary">
+                                      {
+                                        errors.realtInputAndOutPut[index]
+                                          .realInput
+                                      }
+                                    </p>
+                                    <p className="mb-3 pl-1 text-xs text-red-primary">
+                                      {
+                                        errors.realtInputAndOutPut[index]
+                                          .realOutput
+                                      }
+                                    </p>
+                                  </>
+                                ) : null} */}
+                                </>
+                              )
+                            )}
+                          <button
+                            type="button"
+                            className="secondary"
+                            onClick={() => {
+                              push({ realInput: "", realOutput: "" });
+                            }}
+                          >
+                            Add UnVisible Input Testcase
+                          </button>
+                        </div>
+                      </>
+                    )}
+                  </FieldArray>
+                  <p>Is Private</p>
+                  <label htmlFor="isPrivate">
+                    <Field type="checkbox" name="isPrivate" />
+                    {`${values.isPrivate}`}
+                  </label>
+                  <button type="submit">Invite</button>
+                </Form>
+              </>
+            )}
+          </Formik>
+        </div>
       </div>
     </>
   );
--- src/validate/validation.js
+++ src/validate/validation.js
@@ -1,20 +1,28 @@
-import * as yup from 'yup';
-
+import * as yup from "yup";
 
 export const CreateProbSchema = yup.object().shape({
-  Title: yup.string()
+  Title: yup
+    .string()
     .min(3, "Username must be at least 3 characters long!")
     .max(30, "Username must be 30 characters at most!")
     .required("Username is a required field!")
     .strict(),
-  Description: yup.string()
+  Description: yup
+    .string()
     .min(3, "Full name must be at least 3 characters long!")
     .max(256, "Full name must be 34 characters at most!")
     .required("Full name is a required field!"),
-  testInput: yup.array().required("Test Input is a required field!"),
-  testOutput: yup.array().required("Test Output is a required field!"),
+  testInputAndOutPut: yup.array().of(
+    yup.object().shape({
+      testOutput: yup.string().required("testOutput is a required field!"),
+      testInput: yup.string().required("testInput is a required field!"),
+    })
+  ),
   isPrivate: yup.boolean().required("You must set the privacy"),
-  realInput: yup.array().required("Real Input is a required field!"),
-  realOutput: yup.array().required("Real Output is a required field!"),
-  
+  realtInputAndOutPut: yup.array().of(
+    yup.object().shape({
+      realInput: yup.string().required("realInput is a required field!"),
+      realOutput: yup.string().required("realOutput is a required field!"),
+    })
+  ),
 });
