7fbb7ef19ab71334cf6866292a4795dd6476c7c0
thuan <tvc4567@gmail.com>
2022-07-30 23:14:46 +0700

    test2

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -6,7 +6,10 @@ function Comment({ commentId }) {
   const [isSumbit, setIsSubmit] = useState(0);
   const [userList, setUserList] = useState([]);
   const [comment, setComment] = useState("");
-  const [commentList, setcommentList] = useState([])
+  const [commentReply, setCommentReply] = useState("");
+  const [commentList, setcommentList] = useState([]);
+  const [handleShowReply, sethandleShowReply] = useState(false);
+  const [stateReply, setStateReply] = useState(0);
   const getCommentAxios = axios.create({
     withCredentials: true,
   });
@@ -15,15 +18,13 @@ function Comment({ commentId }) {
       await getCommentAxios
         .get(`http://localhost:3001/comment/getComment/${commentId.id}`)
         .then(async (res) => {
-            setGetComment(res.data.getComment)
-            setUserList(res.data.userId)
-            setcommentList(res.data.getComment.comment)
+          setGetComment(res.data.getComment);
+          setUserList(res.data.userId);
+          setcommentList(res.data.getComment.comment);
         });
-  
     };
-      axiosgetComment();
+    axiosgetComment();
   }, [isSumbit]);
-  console.log(commentList)
   const handleComment = async () => {
     await getCommentAxios
       .post("http://localhost:3001/comment/createComment", {
@@ -33,6 +34,21 @@ function Comment({ commentId }) {
       .then(() => {
         setComment("");
         setIsSubmit(isSumbit + 1);
+        sethandleShowReply(false);
+        
+      });
+  };
+  const handleCommentReply = async ({ commentReplyId }) => {
+    await getCommentAxios
+      .post("http://localhost:3001/comment/commentReply", {
+        commentId: getComment._id,
+        comment: commentReply,
+        commentReplyId,
+      })
+      .then(() => {
+        setIsSubmit(isSumbit + 1);
+        setCommentReply("");
+        
       });
   };
   return (
@@ -62,53 +78,82 @@ function Comment({ commentId }) {
               {" "}
               {getComment.comment.map((data, index) => (
                 <>
-                  <div className="flex">
+                  <div className="flex" key={index}>
                     <div className="flex-shrink-0 mr-3"></div>
                     <div className="flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
                       <strong>{userList[index]}</strong>{" "}
                       <span className="text-xs text-gray-400">3:34 PM</span>
                       <p className="text-sm">{data.comment}</p>
-                      <div className="mt-4 flex items-center">
-                        <div className="flex -space-x-2 mr-2"></div>
+                      <strong
+                        onClick={(e) => {
+                          e.preventDefault();
+                          sethandleShowReply(true);
+                          setStateReply(index);
+                        }}
+                        className="cursor-pointer"
+                      >
+                        Reply
+                      </strong>
+                      <div className="">
+                        <div className="">
+                          {handleShowReply === true && index === stateReply ? (
+                            <>
+                              <div className="mb-10 shadow-md w-auto mt-3  antialiased border rounded-lg">
+                                <label className="block mb-2">
+                                  <span className="text-gray-600">
+                                    Add a reply
+                                  </span>
+                                  <textarea
+                                    className="block w-full mt-1 rounded"
+                                    rows="3"
+                                    onChange={(e) =>
+                                      setCommentReply(e.target.value)
+                                    }
+                                    value={commentReply}
+                                  ></textarea>
+                                </label>
+                                <button
+                                  className="px-3 py-2 text-sm text-blue-100 bg-blue-600 rounded"
+                                  onClick={(e) => {
+                                    e.preventDefault();
+                                    handleCommentReply({
+                                      commentReplyId: data.id,
+                                    });
+                                  }}
+                                >
+                                  Reply
+                                </button>
+                              </div>
+                            </>
+                          ) : null}
+                        </div>
                         <div className="text-sm text-gray-500 font-semibold">
+                          {console.log(data)}
                           {data.reply.length > 0 ? (
                             <>Reply: {data.reply.length}</>
                           ) : null}
                           {data.reply.length > 0 ? (
                             <>
                               {" "}
-                              <div className="space-y-4">
-                                <div className="flex">
-                                  <div className="flex-shrink-0 mr-3"></div>
-                                  <div className="flex-1 bg-gray-100 rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
-                                    <strong>Sarah</strong>{" "}
-                                    <span className="text-xs text-gray-400">
-                                      3:34 PM
-                                    </span>
-                                    <p className="text-xs sm:text-sm">
-                                      Lorem ipsum dolor sit amet, consetetur
-                                      sadipscing elitr, sed diam nonumy eirmod
-                                      tempor invidunt ut labore et dolore magna
-                                      aliquyam erat, sed diam voluptua.
-                                    </p>
-                                  </div>
-                                </div>
-                                <div className="flex">
-                                  <div className="flex-shrink-0 mr-3"></div>
-                                  <div className="flex-1 bg-gray-100 rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
-                                    <strong>Sarah</strong>{" "}
-                                    <span className="text-xs text-gray-400">
-                                      3:34 PM
-                                    </span>
-                                    <p className="text-xs sm:text-sm">
-                                      Lorem ipsum dolor sit amet, consetetur
-                                      sadipscing elitr, sed diam nonumy eirmod
-                                      tempor invidunt ut labore et dolore magna
-                                      aliquyam erat, sed diam voluptua.
-                                    </p>
-                                  </div>
-                                </div>
-                              </div>{" "}
+                              {data.reply.map((datareply, index) => (
+                                <>
+                                  {" "}
+                                  <div className="space-y-4 mb-5">
+                                    <div className="flex">
+                                      <div className="flex-shrink-0 mr-3 "></div>
+                                      <div className="flex-1 bg-gray-100 rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
+                                        <strong>{datareply.userName}</strong>{" "}
+                                        <span className="text-xs text-gray-400">
+                                        
+                                        </span>
+                                        <p className="text-xs sm:text-sm">
+                                          {datareply.comment}
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </div>{" "}
+                                </>
+                              ))}
                             </>
                           ) : null}
                         </div>
